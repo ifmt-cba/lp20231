9d6cc117af1f941d0158cf2b419bac507bb3b0c3
Joao Paulo <preti.joao@ifmt.edu.br>
2023-03-08 11:10:37 -0400

    feat: ate exercicio 21 da lista 2

--- exercicios02.py
+++ exercicios02.py
@@ -53,32 +53,110 @@ def questao05:
 #   estatutários. O valor máximo da prestação não poderá ultrapassar 30% do salário
 #   bruto. Faça um programa que permita entrar com o salário bruto
 #   e o valor da prestação e informar se o empréstimo pode ou não ser concedido.
+def questao06():
+    salario_bruto = float(input('Salário Bruto: '))
+    valor_prestacao = float(input('Valor da Prestação: '))
+    credito = salario_bruto * 0.3
+    if (valor_prestacao > credito):
+        print(f'{valor_prestacao} não pode ser superior ao crédito de {credito}!')
+    else:
+        print('Empréstimo concedido!')
 
 #7. Faça um programa que leia um número e indique se o número está compreendido
 #   entre 20 e 50 ou não.
+def questao07():
+    numero = int(input('Número: '))
+    if numero >= 20 and numero <= 50:
+        print('Número compreendido entre 20 e 50')
+    else:
+        print('Número NÃO está compreendido entre 20 e 50')
 
 #8. Faça um programa que leia um número e imprima uma das mensagens:
 #   "Maior do que 20", "Igual a 20"ou "Menor do que 20".
+def questao08():
+    numero = int(input('Número: '))
+    if numero > 20:
+        print('> 20')
+    elif numero < 20:
+        print('< 20')
+    else:
+        print('= 20')    
 
 #9. Faça um programa que permita entrar com o ano de nascimento da pessoa e com o
 #   ano atual. O programa deve imprimir a idade da pessoa. Não se esqueça de
 #   verificar se o ano de nascimento informado é válido.
+def questao09():
+    ano_nascimento = int(input('Ano de Nascimento: '))
+    if ano_nascimento > TODAY.year:
+        print(f'ERRO: Ano de nascimento {ano_nascimento} inválido!')
+    else:
+        idade = TODAY.year - ano_nascimento
+        print(f'Idade: {idade}')
 
 #10. Faça um programa que leia três números inteiros e imprima os três em ordem
 #crescente.
+def questao10():
+    a = int(input('1 Número: '))
+    b = int(input('2 Número: '))
+    c = int(input('3 Número: '))
+
+    if a<=b and b<=c:
+        print(f'{a}\t{b}\t{c}')
+    elif a<=c and c<=b:
+        print(f'{a}\t{c}\t{b}')
+    elif b<=a and a<=c:
+        print(f'{b}\t{a}\t{c}')
+    elif b<=c and c<=a:
+        print(f'{b}\t{c}\t{a}')
+    elif c<=a and a<=b:
+        print(f'{c}\t{a}\t{b}')
+    else:
+        print(f'{c}\t{b}\t{a}')
 
 #11. Faça um programa que leia 3 números e imprima o maior deles.
+def questao11():
+    numero = int(input('1 Número: '))
+    maior = numero
+    numero = int(input('2 Número: '))
+    if numero > maior:
+        maior = numero
+    numero = int(input('3 Número: '))
+    if numero > maior:
+        maior = numero
+    print(f'Maior Número: {maior}')
 
 #12. Faça um programa que leia a idade de uma pessoa e informe:
 #• Se é maior de idade
-#• Se é menor de idade
+#• Se é menor de idadea
 #• Se é maior de 65 anos
+def questao12():
+    idade = int(input('Idade: '))
+    if idade >= 18:
+        print('É maior de idade')
+        if idade > 65:
+            print('Idade maior de 65 anos')
+    else:
+        print('É menor de idade')
 
 #13. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
-#da prova [2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
+#da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
 #a nota da prova 2, a média das notas e uma das mensagens: "Aprovado",
 #"Reprovado"ou "em Prova Final"(a média é 7 para aprovação, menor que 3 para
 #reprovação e as demais em prova final).
+def questao13():
+    nome = input('Nome: ')
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    media = (n1+n2)/2
+    print(f'Nome: {nome}')
+    print(f'Notas: {n1}\t{n2}')
+    print(f'Média: {media}')
+    if media >= 7:
+        print('Aprovado!')
+    elif media >= 3:
+        print('Prova Final!')
+    else:
+        print('Reprovado!')
 
 #14. Faça um programa que permita entrar com o salário de uma pessoa e imprima o
 #desconto do INSS segundo a tabela seguir:
@@ -87,10 +165,30 @@ def questao05:
 #Maior que R$600,00 e menor ou igual a R$1200,00 20%
 #Maior que R$1200,00 e menor ou igual a R$2000,00 25%
 #Maior que R$2000,00 30%
+def questao14():
+    salario = float(input('Salário: '))
+    inss = 0.0
+    if salario > 2000:
+        inss = salario * 0.3
+    elif salario > 1200:
+        inss = salario * 0.25
+    elif salario > 600:
+        inss = salario * 0.2
+    else:
+        inss = 0.0
+    print(f'INSS: {inss}')
 
 #15. Um comerciante comprou um produto e quer vendê-lo com um lucro de 45% se o
 #valor da compra for menor que R$20,00, caso contrário, o lucro será de 30%.
 #Faça um programa que leia o valor do produto e imprima o valor da venda.
+def questao15():
+    valor_produto = float(input('Custo do produto: '))
+    lucro = 0.0
+    if valor_produto < 20:
+        lucro = valor_produto * 0.45 
+    else:
+        lucro = valor_produto * 0.3
+    print(f'Valor de Venda: {valor_produto + lucro}')
 
 #16. A confederação brasileira de natação irá promover eliminatórias para o
 #próximo mundial. Faça um programa que receba a idade de um nadador e imprima
@@ -101,6 +199,20 @@ def questao05:
 #Juvenil A 11 - 13 anos
 #Juvenil B 14 - 17 anos
 #Sênior maiores de 18 anos
+def questao16():
+    idade = int(input('Idade: '))
+    if idade >= 18:
+        print('Categoria Sênior!')
+    elif idade >= 14:
+        print('Categoria Juvenil B!')
+    elif idade >= 11:
+        print('Categoria Juvenil A!')
+    elif idade >= 8:
+        print('Categoria Infantil B!')
+    elif idade >= 5:
+        print('Categoria Infantil A!')
+    else:
+        print('Não pode participar do campeonato!')
 
 #17. Depois da liberação do governo para as mensalidades dos planos de saúde,
 #as pessoas começaram a fazer pesquisas para descobrir um bom plano, não
@@ -114,10 +226,55 @@ def questao05:
 #Acima de 45 até 59 anos R$150,00
 #Acima de 59 até 65 anos R$250,00
 #Maior que 65 anos R$400,00
+def questao17():
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    print('Valor do plano de saúde: ')
+    if idade >= 65:
+        print(400)
+    elif idade >= 59:
+        print(250)
+    elif idade >= 45:
+        print(150)
+    elif idade >= 29:
+        print(120)
+    elif idade >= 10:
+        print(60)
+    else:
+        print(30)
 
 #18. Faça um programa que leia um número inteiro entre 1 e 12 e escreva o mês
 #correspondente. Caso o usuário digite um número fora desse intervalo, deverá
 #aparecer uma mensagem informando que não existe mês com este número.
+def questao18():
+    mes = int(input('Mês: '))
+    if mes < 1 or mes > 12:
+        print('Mês inválido!')
+    else:
+        if mes == 1:
+            print('Janeiro')
+        elif mes == 2:
+            print('Fevereiro')
+        elif mes == 3:
+            print('Março')
+        elif mes == 4:
+            print('Abril')
+        elif mes == 5:
+            print('Maio')
+        elif mes == 6:
+            print('Junho')
+        elif mes == 7:
+            print('Julho')
+        elif mes == 8:
+            print('Agosto')
+        elif mes == 9:
+            print('Setembro')
+        elif mes == 10:
+            print('Outubro')
+        elif mes == 11:
+            print('Novembro')
+        else:
+            print('Dezembro')
 
 #19. Em um campeonato nacional de arco-e-flecha, tem-se equipes de três jogadores
 #para cada estado. Sabendo-se que os arqueiros de uma equipe não obtiveram o
@@ -127,6 +284,29 @@ def questao05:
 #• Mostrar esses valores em ordem decrescente;
 #• Se a soma dos pontos for maior do que 100, imprimir a média aritmética entre eles,
 #  caso contrário, imprimir a mensagem "Equipe desclassificada".
+def questao19():
+    a = int(input('Pontos J1: '))
+    b = int(input('Pontos J2: '))
+    c = int(input('Pontos J3: '))
+
+    if a>=b and b>=c:
+        print(f'{a}\t{b}\t{c}')
+    elif a>=c and c>=b:
+        print(f'{a}\t{c}\t{b}')
+    elif b>=a and a>=c:
+        print(f'{b}\t{a}\t{c}')
+    elif b>=c and c>=a:
+        print(f'{b}\t{c}\t{a}')
+    elif c>=a and a>=b:
+        print(f'{c}\t{a}\t{b}')
+    else:
+        print(f'{c}\t{b}\t{a}')
+    
+    soma = a+b+c
+    if soma > 100:
+        print(f'Média de pontos por jogador: {soma/3.0}')
+    else:
+        print('Equipe desclassificada')
 
 #20. O banco XXX concederá um crédito especial com juros de 2% aos seus clientes de
 #acordo com o saldo médio no último ano. Faça um programa que leia o saldo médio
@@ -138,6 +318,16 @@ def questao05:
 #de 501 a 1000 30% do valor do saldo médio
 #de 1001 a 3000 40% do valor do saldo médio
 #acima de 3001 50% do valor do saldo médio
+def questao20():
+    saldo_medio = float(input('Saldo Médio: '))
+    if saldo_medio > 3001:
+        print(f'Crédito: {saldo_medio * 0.5}')
+    elif saldo_medio >= 1001:
+        print(f'Crédito: {saldo_medio * 0.4}')
+    elif saldo_medio >= 501:
+        print(f'Crédito: {saldo_medio * 0.3}')
+    else:
+        print('Nenhum crédito disponível')        
 
 #21. A biblioteca de uma Universidade deseja fazer um programa que leia o nome do
 #livro que será emprestado, o tipo de usuário (professor ou aluno) e possa
@@ -146,10 +336,20 @@ def questao05:
 #• Nome do livro:
 #• Tipo de usuário:
 #• Total de dias:
+def questao21():
+    nome = input('Nome do Livro: ')
+    tipo = input('Tipo A/P (Aluno ou Professor): ')
+    if tipo == 'A':
+        print(f'Total de dias: 3')
+    elif tipo == 'P':
+        print(f'Total de dias: 10')
+    else:
+        print('Não pode ser emprestado!')
 
 #22. Construa um programa que leia o percurso em quilômetros, o tipo do carro e
 #informe o consumo estimado de combustível, sabendo-se que um carro tipo A faz
 #12 km com um litro de gasolina, um tipo B faz 9 km e o tipo C 8 km por litro.
+
 
 #23. Crie um programa que informe a quantidade total de calorias de uma refeição
 #a partir da escolha do usuário que deverá informar o prato, a sobremesa, e
