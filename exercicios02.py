c08ec5aa73adfe38a551fbbb1e58e1747e2229c4
Joao Paulo <preti.joao@ifmt.edu.br>
2023-03-08 15:10:12 -0400

    fix: questao 18 da lista 2

--- exercicios02.py
+++ exercicios02.py
@@ -251,30 +251,8 @@ def questao18():
     if mes < 1 or mes > 12:
         print('Mês inválido!')
     else:
-        if mes == 1:
-            print('Janeiro')
-        elif mes == 2:
-            print('Fevereiro')
-        elif mes == 3:
-            print('Março')
-        elif mes == 4:
-            print('Abril')
-        elif mes == 5:
-            print('Maio')
-        elif mes == 6:
-            print('Junho')
-        elif mes == 7:
-            print('Julho')
-        elif mes == 8:
-            print('Agosto')
-        elif mes == 9:
-            print('Setembro')
-        elif mes == 10:
-            print('Outubro')
-        elif mes == 11:
-            print('Novembro')
-        else:
-            print('Dezembro')
+        data = datetime(2022, mes, 1)
+        print(data.strftime('%b'))
 
 #19. Em um campeonato nacional de arco-e-flecha, tem-se equipes de três jogadores
 #para cada estado. Sabendo-se que os arqueiros de uma equipe não obtiveram o
